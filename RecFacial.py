cff7e7db3a9cba36bda74063baaa10841fabe2c2
Antonio <joseantoniomartinezmilanez@gmail.com>
2024-04-23 23:13:55 -0600

    Modificacion en la invocacion del archivo prueba

--- RecFacial.py
+++ RecFacial.py
@@ -1,15 +1,26 @@
 """
 Proyecto de reconocimiento facial
 """
-import time
+#import time
 import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
-from prueba import AbrirRec 
 
 """
+*******************************************
+Invocacion para funciones
+Invocar el abrir del reconocimiento facial
+*******************************************
+"""
+def AbrirRecF():
+    exec(open("prueba.py").read())
+    print("Invocacion al rec")
+
+"""
+*******************************************
 Configuracion de la pantalla
 Color, imagen, tamaño, etc.
+*******************************************
 """
 interfaz = tk.Tk()
 interfaz.title("Inicio de sesión")
@@ -34,16 +45,18 @@ img2.image = render
 img2.place(x=555, y=0)
 
 """
+*******************************************
 Implementacion de elementos
 en la interfaz
 Botones, Input, Labels, etc.
+*******************************************
 """
 Tx1 = Label(interfaz, text="Usuario", fg="White", font=("Arial", 20), bg="#002C65").place(x=100, y=270) #fg es para el color del texto
 
 cTexto = tk.Entry(interfaz, font=("Arial",16))
 cTexto.place(x=100, y=310)
 b1 = tk.Button(interfaz, text="Iniciar\nReconocimiento", width=13, height=2, 
-    bg="#EF7F01", font=("Arial Black",12), borderwidth=5, command=AbrirRec) ##EF7F01 para el color naranja, #002C65 para el color azul
+    bg="#EF7F01", font=("Arial Black",12), borderwidth=5, command=AbrirRecF) ##EF7F01 para el color naranja, #002C65 para el color azul
 b1.place(x=200, y=400)
 
 input()
--- prueba.py
+++ prueba.py
@@ -1,14 +1,12 @@
 import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
-from prueba import * 
 
-def AbrirRec():
-    def cerrar():
-        NuevaI.quit()
-        NuevaI.destroy()
-    NuevaI = tk.Tk()
-    NuevaI.title("Reconocimiento facial")
-    NuevaI.geometry("800x600")
+def cerrar():
+    NuevaI.quit()
+    NuevaI.destroy()
 
-    b1 = Button(NuevaI,text="Cerrar", command= cerrar).place(x=100, y=100)
+NuevaI = tk.Tk()
+NuevaI.title("Reconocimiento facial")
+NuevaI.geometry("800x600")
+b1 = Button(NuevaI,text="Cerrar", command= cerrar).place(x=100, y=100)
